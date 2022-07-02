5d77b579b48b69cf8295a824442b4e5dfdc79fb5
niraya-creative <niraya.creative@gmail.com>
2022-07-02 15:53:09 +0900

    Answer: section04 step02

--- skeleton/section04/step02/main.go
+++ skeleton/section04/step02/main.go
@@ -7,12 +7,16 @@ func main() {
 }
 
 func start() {
-	// TODO: 値を入力する関数を呼び出し変数fromに代入
-
+	from := inputValue("摂氏[°C] -> 華氏[°F]", "°C")
 	to := from*1.8 + 32
 	fmt.Printf("結果: %.2f[°F]\n", to)
 }
 
 // TODO: 値を入力する関数を定義する
-// 引数：name（値の名前）, fromUnit（変換元の単位）
-// 戻り値：入力された値
+func inputValue(name, fromUnit string) float64 {
+	fmt.Println(name)
+	var v float64
+	fmt.Print("変換する値[°C]>")
+	fmt.Scanln(&v)
+	return v
+}
