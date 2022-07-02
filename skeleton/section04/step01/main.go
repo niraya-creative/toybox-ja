63822fd406470c9b2481f489d980b27a6817357a
niraya-creative <niraya.creative@gmail.com>
2022-07-02 15:20:18 +0900

    Answer: section04 step01

--- skeleton/section04/step01/main.go
+++ skeleton/section04/step01/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: 以下の処理をstart関数を定義し移動させる
+	start()
+}
+
+// TODO: startという関数を定義
+func start() {
 	fmt.Println("摂氏[°C] -> 華氏[°F]")
 	var from float64
 	fmt.Print("変換する値[°C]>")
@@ -11,8 +15,4 @@ func main() {
 
 	to := from*1.8 + 32
 	fmt.Printf("結果: %.2f[°F]\n", to)
-
-	// TODO: 関数startを呼び出す
 }
-
-// TODO: startという関数を定義
