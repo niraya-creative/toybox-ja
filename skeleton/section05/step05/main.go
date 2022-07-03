7ab5d7643e325dc64e6f733b574d2d03a18fe131
niraya-creative <niraya.creative@gmail.com>
2022-07-03 13:32:12 +0900

    Answer: section05 step05

--- skeleton/section05/step05/main.go
+++ skeleton/section05/step05/main.go
@@ -12,9 +12,7 @@ var (
 )
 
 func init() {
-	// TODO: 変数flagOutputDirにdirというオプションを設定する
-	// デフォルト値は"."、説明は"出力先のディレクトリ"
-
+	flag.StringVar(&flagOutputDir, "dir", ".", "出力先のディレクトリ")
 }
 
 func main() {
@@ -52,8 +50,7 @@ func main() {
 			showItems(items)
 		case 3: // 集計
 			summaries := ab.GetSummaries()
-			// TODO: 出力先ディレクトリとファイル名"summary.csv"を結合して変数fileに入れる
-
+			file := filepath.Join(flagOutputDir, "summary.csv")
 			SaveSummary(file, summaries)
 			fmt.Println(file, "を出力しました")
 		case 4: // 終了
